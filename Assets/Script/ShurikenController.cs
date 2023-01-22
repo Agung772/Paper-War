6a745ae50682b400338b8f852f6d2d3db78b9a9f
Agung Gunawan <agg.nst772@gmail.com>
2023-01-22 14:34:41 +0700

    update shuriken

--- Assets/Script/ShurikenController.cs
+++ Assets/Script/ShurikenController.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class ShurikenController : MonoBehaviour
 {
     public new Rigidbody rigidbody;
+    public new ParticleSystem particleSystem;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("GroundDarat") || other.CompareTag("GroundAir") || other.CompareTag("Player"))
         {
             rigidbody.isKinematic = true;
+            particleSystem.transform.parent = null;
+            particleSystem.Play();
+
+            Destroy(gameObject);
         }
 
         if (other.CompareTag("Player"))
